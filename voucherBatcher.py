f39db294dbfab23c456e1dd68835efffc9d4ff4e
ameliasutton <ameliasutton@umass.edu>
2021-12-09 08:16:24 -0500

    Implemented selectMostRecentBatch and updated get Batch Id to select most recent batch if dates fail to match.

--- voucherBatcher.py
+++ voucherBatcher.py
@@ -40,7 +40,6 @@ class VoucherBatchRetriever:
         self.voucherId = self.getVoucherId()
 
     # Returns Batch Group ID matching Batch Group Name provided in config file
-    # TODO: Have this give an error response if Batch Group Name does not match
     def getBatchGroupId(self):
         response = self.requester.singleRequest("batch-groups?query=name=\"" + self.batchGroup + "\"", self.session)
         if response["totalRecords"] == 0:
@@ -52,30 +51,22 @@ class VoucherBatchRetriever:
     # Updates config file with currently selected Start Date, End Date, and API Token
     def updateConfig(self):
         with open(self.configFileName, "r") as readConf:
-            configJson = json.load(readConf)
-            configJson["batchEndDate"] = self.batchEndDate
-            configJson["batchStartDate"] = self.batchStartDate
-            configJson["token"] = self.requester.token
+            config = json.load(readConf)
+            config["batchEndDate"] = self.batchEndDate
+            config["batchStartDate"] = self.batchStartDate
+            config["token"] = self.requester.token
         with open(self.configFileName, "w") as writeConf:
-            writeConf.write(json.dumps(configJson, indent=4))
+            writeConf.write(json.dumps(config, indent=4))
 
     # Returns the Batch ID associated with the selected Dates
-    # TODO: Modify this to use the "selectMostRecentBatch" function for efficiency
-    # TODO: and ease of use.
     def getBatchId(self):
         batch = self.requester.singleRequest("batch-voucher/batch-voucher-exports?query=(batchGroupId==\""
                                              + self.batchGroupId + "\" AND start==\"" + self.batchStartDate
                                              + "\" AND end==\"" + self.batchEndDate + "\")&sort(-metadata.updatedDate)",
                                              self.session)
         if batch["totalRecords"] == 0:
-            print("No Batch found matching current start and end dates")
-            batch = self.requester.singleRequest("batch-voucher/batch-voucher-exports?query=batchGroupId==\""
-                                                  + self.batchGroupId + "\"", self.session)
-            print("Selecting New Batch Dates...")
-            self.batchEndDate = batch["batchVoucherExports"][-1]["end"][0:23]+"*"
-            self.batchStartDate = batch["batchVoucherExports"][-1]["start"][0:23]+"*"
-            self.getBatchId()
-            print("New Batch Dates Selected")
+            print("Selecting Most Recent Batch Date...")
+            self.selectMostRecentBatch()
             return
         batch = batch["batchVoucherExports"][0]["id"]
         return batch
@@ -110,13 +101,25 @@ class VoucherBatchRetriever:
             self.batchStartDate = batch["batchVoucherExports"][0]["start"][0:23] + "*"
             print("Next Batch Selected\n"
                   "Run Date: " + self.batchEndDate)
+            self.updateConfig()
         return
 
     # Moves Start and End Dates to point towards the most recently run Batch
-    # TODO: Utilize the 'paging' function to retrieve the last page of responses,
-    # TODO: then use "selectPreviousVoucher" to ensure we have the correct result.
     def selectMostRecentBatch(self):
-        print("Function Not Yet Implemented")
+        batch = None
+        for page in self.requester.paging("batch-voucher/batch-voucher-exports?query=batchGroupId==\""
+                                          + self.batchGroupId + "\"", 100, self.session):
+            last_page = batch
+            batch = page
+        if batch["totalRecords"] == 0:
+            batch = last_page
+        selected_batch = batch["batchVoucherExports"][-1]
+        self.batchId = selected_batch["id"]
+        self.batchEndDate = selected_batch["end"][0:23] + "*"
+        self.batchStartDate = selected_batch["start"][0:23] + "*"
+        print("Most Recent Batch Selected\n"
+              "Run Date: " + self.batchEndDate)
+        self.updateConfig()
 
     # Moves Start and End Dates to point towards the previous Batch
     def selectPreviousBatch(self):
@@ -131,6 +134,7 @@ class VoucherBatchRetriever:
             self.batchStartDate = batch["batchVoucherExports"][0]["start"][0:23] + "*"
             print("Previous Batch Selected\n"
                   "Run Date: " + self.batchEndDate)
+            self.updateConfig()
         return
 
     # Returns a dict of the Batched Vouchers
