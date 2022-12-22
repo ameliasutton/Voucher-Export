a76ceb8f21653f65ed7618bf648a7a59d0cd5934
ameliasutton <ameliasutton@umass.edu>
2022-12-22 10:58:34 -0500

    Updated Filepaths

---
+++ update_voucher_export_excecutable.bat
@@ -0,0 +1,2 @@
+pyinstaller "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source\main.py" -F -n VoucherExport --distpath "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source" --workpath "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source\exe_build" --noconfirm
+pause
