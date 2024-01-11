5e841cae28060a1121e8af8263cbf0c2068b8935
ameliasutton <ameliasutton@umass.edu>
2024-01-11 16:00:08 -0500

    depricated

--- update_voucher_export_excecutable.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source\main.py" -F -n VoucherExport --distpath "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source" --workpath "C:\Users\ameliasutton\Python Projects\Voucher-Export-Source\exe_build" --noconfirm
-pause
